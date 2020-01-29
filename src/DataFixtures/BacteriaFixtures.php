a72bda406ca7565fd8a6c0d13f144c73c99df9dd
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2020-01-29 12:01:39 +0100

    add assets for bacteria avatar

--- src/DataFixtures/BacteriaFixtures.php
+++ src/DataFixtures/BacteriaFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Bacteria;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
 
-class BacteriaFixtures extends Fixture
+class BacteriaFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 10; $i++) {
+            $bacteria = new Bacteria();
+            $bacteria->setName($faker->colorName);
+            $bacteria->setAvatar();
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDependencies()
+    {
+        // TODO: Implement getDependencies() method.
     }
 }
