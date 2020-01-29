fc5d2db7f6c783aa1d9e6939ded8f1c811cdee47
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2020-01-29 14:28:01 +0100

    add validation back

--- src/Entity/Bacteria.php
+++ src/Entity/Bacteria.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
@@ -20,11 +21,15 @@ class Bacteria
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank
+     * @Assert\Regex("/^\w+/")
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Regex("/^\w+/ .")
      */
     private $avatar;
 
--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
@@ -22,6 +23,8 @@ class Team
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank()
+     * @Assert\Regex("/^\w+/ .")
      */
     private $name;
 
